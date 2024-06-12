ef4251dd0e3aa2851d9072f2896500410866e1b6
Shehan Lakshitha <shehanlaksh@gmail.com>
2024-06-12 12:25:47 +0530

    First commit

---
+++ src/constants/colors.js
@@ -0,0 +1,14 @@
+const COLORS = {
+    white: '#FFF',
+    black: '#000',
+    primary: '#9747FF',
+    purple:"#8A86EA",
+    warning:'#FF0000',
+    green: '#006400',
+    low_purple:"#B9AAEE",
+    low_grey:'#E3E1D9',
+    subText:"#A8A8A8",
+
+}
+
+export default COLORS;
